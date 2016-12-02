779c50b39f3e2498c17def476104cd24cbacc610
TheScarastic <warabhishek@gmail.com>
2016-12-02 02:48:41 -0500

    sdm845-common: Add assertions on modem version
     * Based off android_device_xiaomi_msm8996-common
    
    Change-Id: Ib578c77492cc3060915b12fad10d023b3ab38875

--- sdm845.mk
+++ sdm845.mk
@@ -68,6 +68,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml \
     $(LOCAL_PATH)/configs/qti_whitelist.xml:system/etc/sysconfig/qti_whitelist.xml
 
+# Recovery
+PRODUCT_PACKAGES += \
+    librecovery_updater_xiaomi
+
 # VNDK-SP
 PRODUCT_PACKAGES += \
     vndk-sp
