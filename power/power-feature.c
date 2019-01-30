22d930e87c438a8d661fd7d66adea84e0244ab06
TheScarastic <warabhishek@gmail.com>
2019-01-30 18:16:42 +0530

    sdm845-common: power: Support event-based dt2w node
    Change-Id: I08c874f3ca50fad1db820ddd85ef0f5d7ccf0ed2

--- power/power-feature.c
+++ power/power-feature.c
@@ -14,10 +14,65 @@
  * limitations under the License.
  */
 
+#include <dirent.h>
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
 #include "power-feature.h"
 
+#include <hardware/power.h>
+#include <linux/input.h>
 #include <log/log.h>
 
+int open_ts_input() {
+    int fd = -1;
+    DIR *dir = opendir("/dev/input");
+
+    if (dir != NULL) {
+        struct dirent *ent;
+
+        while ((ent = readdir(dir)) != NULL) {
+            if (ent->d_type == DT_CHR) {
+                char absolute_path[PATH_MAX] = {0};
+                char name[80] = {0};
+
+                strcpy(absolute_path, "/dev/input/");
+                strcat(absolute_path, ent->d_name);
+
+                fd = open(absolute_path, O_RDWR);
+                if (ioctl(fd, EVIOCGNAME(sizeof(name) - 1), &name) > 0) {
+                    if (strcmp(name, "atmel_mxt_ts") == 0 || strcmp(name, "fts") == 0 ||
+                            strcmp(name, "ft5x46") == 0 || strcmp(name, "synaptics_dsx") == 0 ||
+                            strcmp(name, "NVTCapacitiveTouchScreen") == 0)
+                        break;
+                }
+
+                close(fd);
+                fd = -1;
+            }
+        }
+
+        closedir(dir);
+    }
+
+    return fd;
+}
+
 void set_device_specific_feature(feature_t feature, int state) {
-    // stub
+    switch (feature) {
+        case POWER_FEATURE_DOUBLE_TAP: {
+            int fd = open_ts_input();
+            if (fd == -1) {
+                ALOGW("DT2W won't work because no supported touchscreen input devices were found");
+                return;
+            }
+            struct input_event ev;
+            ev.type = EV_SYN;
+            ev.code = SYN_CONFIG;
+            ev.value = state ? INPUT_EVENT_WAKUP_MODE_ON : INPUT_EVENT_WAKUP_MODE_OFF;
+            write(fd, &ev, sizeof(ev));
+            close(fd);
+        }
+    }
 }
