7d323d208d674c6e2af06237e1322199d9e84469
Mesquita <mickaelmendes50@gmail.com>
2020-08-06 02:05:03 +0000

    sdm845-common: Sync architecture flags with skunk device
    * Required after https://gerrit.aospa.co/c/AOSPA/android_art/+/13461
    
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Change-Id: I25be223faff903e5627feee2840863cc9c9dbb5c

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -11,10 +11,10 @@ COMMON_PATH := device/xiaomi/sdm845-common
 
 # Architecture
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_ABI2 :=
-TARGET_CPU_VARIANT := cortex-a75
+TARGET_CPU_VARIANT := kryo300
 
 TARGET_2ND_ARCH := arm
 TARGET_2ND_ARCH_VARIANT := armv8-2a
